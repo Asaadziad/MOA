2d0c85fc5bfbe3d4004df4c42f86ec5b6768a0e7
Asaadziad <asaadabuelhija@gmail.com>
2024-02-12 21:56:15 +0200

    .

--- engine.cpp
+++ engine.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <raylib.h>
-
+#include <string>
 
 extern "C" {
    void engine_update() {
     BeginDrawing();
     ClearBackground(RED);
-    DrawText("Hello loser",0,0,25,RAYWHITE); 
+    
+    const char* fps = ("FPS: "+ std::to_string(GetFPS())).c_str();
+    DrawText(fps, 0, 0, 25, RAYWHITE); 
+    DrawText("hello world", 250,250,25,RAYWHITE); 
     EndDrawing(); 
   }
 }
